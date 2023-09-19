b48bdfe0391c9b4995bf629826088cd28420e8d9
Adimora Uju Louisa <adimorauju@gmail.com>
2023-09-19 13:04:01 +0000

    markup and skill continues here

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,16 @@ $(document).ready(function() {
         dropRadius: 10
     });
 });
+
+
+const bars = document.querySelectorAll('.progress_bar');
+
+bars.forEach(function(bar) {
+    const percentage = bar.dataset.percent;
+    const tooltip = bar.querySelector('.tooltip'); // Use querySelector for the tooltip
+
+    tooltip.innerText = percentage + '%';
+    bar.style.width = percentage + '%';
+
+    console.log(percentage);
+});
