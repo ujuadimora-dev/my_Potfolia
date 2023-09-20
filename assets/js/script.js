3d9aaf4e6da71d389f5c5370fa4eb2bfaab3c37f
Adimora Uju Louisa <adimorauju@gmail.com>
2023-09-20 02:47:19 +0000

    Mark up for the Counter and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,49 @@
-$(document).ready(function() {
 
-    
+$(document).ready(function() {
+    // Ripples Effect
     $('#profile_ripples').ripples({
         resolution: 512,
         dropRadius: 10
     });
-});
 
+    // Progress Bar
+    const bars = document.querySelectorAll('.progress_bar');
 
-const bars = document.querySelectorAll('.progress_bar');
+    bars.forEach(function(bar) {
+        const percentage = bar.dataset.percent;
+        const tooltip = bar.querySelector('.tooltip');
 
-bars.forEach(function(bar) {
-    const percentage = bar.dataset.percent;
-    const tooltip = bar.querySelector('.tooltip'); // Use querySelector for the tooltip
+        tooltip.innerText = percentage + '%';
+        bar.style.width = percentage + '%';
 
-    tooltip.innerText = percentage + '%';
-    bar.style.width = percentage + '%';
+        console.log(percentage);
+    });
 
-    console.log(percentage);
+    // Counter
+    const counters = document.querySelectorAll('.counter');
+
+    function runCounter() {
+        counters.forEach(counter => {
+            counter.innerText = 0;
+
+            let target = +counter.dataset.count;
+
+            let step = target / 100;
+
+        
+            let countIt = function() {
+                let displayedCount = +counter.innerText;
+                if (displayedCount < target) {
+
+                    counter.innerText = Math.ceil(displayedCount + step);
+                    setTimeout(countIt, 1); 
+                } else {
+                    counter.innerText = target;
+                }
+            }
+            countIt();
+        });
+    }
+
+    runCounter(); 
 });
